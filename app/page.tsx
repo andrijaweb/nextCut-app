acf8a63bcf880b448077af7a09efac02be6b8ba8
andrijaweb <andrijawebd@gmail.com>
2024-07-31 22:12:50 +0200

    Added login form page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
+import LoginForm from "@/components/forms/LoginForm";
+import Image from "next/image";
+
 export default function Homepage() {
   return (
-    <div>
-      <h1 className="text-yellow-500">Homepage</h1>
+    <div className="flex h-screen max-h-screen">
+      <section className="container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/imgs/logo.svg"
+            width={1000}
+            height={1000}
+            alt="NextCut"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <LoginForm />
+
+          <div className="text-textGray-500 mt-20">
+            <p>© NextCut | All rights reserved</p>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/imgs/barber.png"
+        width={1000}
+        height={1000}
+        alt="barbershop interior"
+        className="rounded-tl-lg rounded-bl-lg side-img max-w-[50%]"
+      />
     </div>
   );
 }
