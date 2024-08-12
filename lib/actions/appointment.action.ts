7e3426d3513851aa6831a045b89c582d8afedcd8
andrijaweb <andrijawebd@gmail.com>
2024-08-12 23:36:42 +0200

    Added new appointment action file, added createAppointment function

---
+++ lib/actions/appointment.action.ts
@@ -0,0 +1,25 @@
+import { CreateAppointmentParams } from "@/types";
+import { createAdminClient } from "../appwrite.config";
+import { ID } from "node-appwrite";
+import { parseStringify } from "../utils";
+
+const { DATABASE_ID, APPOINTMENT_COLLECTION_ID } = process.env;
+
+export const createAppointment = async (
+  newAppointment: CreateAppointmentParams
+) => {
+  try {
+    const { database } = await createAdminClient();
+
+    const appointment = await database.createDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      ID.unique(),
+      newAppointment
+    );
+
+    return JSON.parse(JSON.stringify(appointment));
+  } catch (err) {
+    console.error("Failed to create appointment:", err);
+  }
+};
