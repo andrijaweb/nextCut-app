186c0df47cd36a612991fe478679ab7cbac30211
andrijaweb <andrijawebd@gmail.com>
2024-08-19 21:11:33 +0200

    Added function for getting appointment by appointment id

--- lib/actions/appointment.action.ts
+++ lib/actions/appointment.action.ts
@@ -3,6 +3,7 @@
 import { CreateAppointmentParams } from "@/types";
 import { createAdminClient } from "../appwrite.config";
 import { ID } from "node-appwrite";
+import { parseStringify } from "../utils";
 
 const { DATABASE_ID, APPOINTMENT_COLLECTION_ID } = process.env;
 
@@ -19,8 +20,27 @@ export const createAppointment = async (
       newAppointment
     );
 
-    return JSON.parse(JSON.stringify(appointment));
+    return parseStringify(appointment);
   } catch (err) {
     console.error("Failed to create appointment:", err);
+  }
+};
+
+export const getAppointment = async (appointmentId: string) => {
+  try {
+    const { database } = await createAdminClient();
+
+    const appointment = await database.getDocument(
+      DATABASE_ID!,
+      APPOINTMENT_COLLECTION_ID!,
+      appointmentId
+    );
+
+    return parseStringify(appointment);
+  } catch (error) {
+    console.error(
+      "An error occurred while retrieving the existing patient:",
+      error
+    );
   }
 };
