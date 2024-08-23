819df09334dde0c845ceba90a819753c6003501f
andrijaweb <andrijawebd@gmail.com>
2024-08-23 21:04:11 +0200

    Added account route, added layout and root page for account

---
+++ app/(root)/account/page.tsx
@@ -0,0 +1,13 @@
+import AccountNavigation from "@/components/AccountNavigation";
+
+const AccountPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold mb-5">Account Settings</h1>
+
+      <AccountNavigation />
+    </div>
+  );
+};
+
+export default AccountPage;
